03164c331496fa7afaa3d8315dba293941fd2cb7
Chris Penner <christopher.penner@gmail.com>
2019-09-25 10:06:48 -0600

    Add TicTacTwo

--- src/TicTacTwo.hs
+++ src/TicTacTwo.hs
@@ -1,63 +1,69 @@
 module TicTacTwo where
 
-data Board a = Board
-  a a
-  a a
-  deriving (Show, Eq)
-
 data Piece = O | X
   deriving (Show, Eq)
 
-emptyBoard :: Board (Maybe Piece)
-emptyBoard =
-  Board
-    Nothing Nothing
-    Nothing Nothing
+data Grid a =
+    Grid
+      a a
+      a a
+  deriving (Show)
+
+emptyGrid :: Grid (Maybe a)
+emptyGrid =
+    Grid
+        Nothing Nothing
+        Nothing Nothing
+
+-- data Maybe a = Just a | Nothing
 
 data Coord = I | II
+  deriving Show
 
-playPiece :: Piece -> (Coord, Coord) -> Board (Maybe Piece) -> Board (Maybe Piece)
-playPiece p (I, I) (Board _ b c d) = Board (Just p) b c d
-playPiece p (II, I) (Board a _ c d) = Board a (Just p) c d
-playPiece p (I, II) (Board a b _ d) = Board a b (Just p) d
-playPiece p (II, II) (Board a b c _) = Board a b c (Just p)
+placePiece :: Coord -> Coord -> Piece -> Grid (Maybe Piece) -> Maybe (Grid (Maybe Piece))
+placePiece x y p (Grid a b c d) =
+    case (x, y) of
+        (I, I) | a == Nothing -> Just $ Grid (Just p) b c d
+        (I, II) | b == Nothing -> Just $ Grid a (Just p) c d
+        (II, I) | c == Nothing -> Just $ Grid a b (Just p) d
+        (II, II) | d == Nothing -> Just $ Grid a b c (Just p)
+        _ -> Nothing
 
-checkWin :: Board (Maybe Piece) -> Maybe Piece
-checkWin (Board
-  (Just a) _
-  _        (Just d))
-    | a == d = Just a
-checkWin (Board
-  (Just a) (Just b)
-  _        _)
-    | a == b = Just a
-checkWin (Board
-  (Just a) _
-  (Just c) _)
-    | a == c = Just a
-checkWin (Board
-  _ (Just b)
-  _ (Just d))
-    | b == d = Just b
+checkWin :: Grid (Maybe Piece) -> Maybe Piece
+checkWin (Grid (Just a) (Just b) _ _) | a == b = Just a
+checkWin (Grid _ _ (Just c) (Just d)) | c == d = Just c
+checkWin (Grid (Just a) _ (Just c) _) | a == c = Just a
+checkWin (Grid _ (Just b) _ (Just d)) | b == d = Just b
 checkWin _ = Nothing
 
-parseCoord :: String -> Maybe Coord
-parseCoord "I" = Just I
-parseCoord "II" = Just II
-parseCoord _ = Nothing
+parseLine :: String -> Maybe (Coord, Coord)
+parseLine s =
+    case words s of
+        ["I", "I"] -> Just (I, I)
+        ["II", "I"] -> Just (II, I)
+        ["I", "II"] -> Just (I, II)
+        ["II", "II"] -> Just (II, II)
+        _ -> Nothing
 
-loop :: Piece -> Board (Maybe Piece) -> IO ()
-loop p board = do
-    putStrLn "Where would you like to play? e.g. I II"
-    ln <- getLine
-    case traverse parseCoord (words ln) of
-        Just [x, y] -> do
-            let newBoard = playPiece p (x, y) board
-            case checkWin newBoard of
-                Just p -> putStrLn $ show p <> " wins!"
-                Nothing -> loop (nextPiece p) newBoard
-        _ -> loop p board
 
-nextPiece X = O
-nextPiece O = X
+loop :: Piece -> Grid (Maybe Piece) -> IO ()
+loop player grid = do
+    putStrLn ("Player: " <> show player <> " Where do you want to play?")
+    input <- getLine
+    case parseLine input of
+        Nothing -> loop player grid
+        Just (x, y) ->
+            case placePiece  x y player grid of
+                Nothing -> loop player grid
+                Just newGrid -> do
+                    case checkWin newGrid of
+                        Just winner -> putStrLn (show winner <> " WON!")
+                        Nothing -> loop (nextPlayer player) newGrid
+
+nextPlayer :: Piece -> Piece
+nextPlayer X = O
+nextPlayer O = X
+
+main :: IO ()
+main = loop O emptyGrid
 
